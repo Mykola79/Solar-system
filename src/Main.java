c99545ad53b4d73f6f47c5b04e9041ea1fcb57ec
Mykola79 <mykola.rusyn79@gmail.com>
2023-03-29 12:48:48 -0400

    developer1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem2 solar=new SolarSystem2();
+
+        solar.planets="2";
+        solar.moon="2";
+        solar.sun="1";
+    }
+
+}
---
+++ src/SolarSystem2.java
@@ -0,0 +1,5 @@
+public class SolarSystem2 {
+    String planets;
+    String moon;
+    String sun;
+}
